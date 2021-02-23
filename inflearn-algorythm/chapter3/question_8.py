9d4f32de06014fbb8225c0d00e1ffe73bbc29cf0
eoc940 <hgk0000@naver.com>
2021-02-23 21:53:03 +0900

    feat: 곳감(모래시계) 문제 수정

--- inflearn-algorythm/chapter3/question_8.py
+++ inflearn-algorythm/chapter3/question_8.py
@@ -4,34 +4,31 @@ n = int(input())
 a = [list(map(int, input().split())) for _ in range(n)]
 
 m = int(input())
-b = [list(map(int, input().split())) for _ in range(m)]
-
-def rotate(li, gam) :
-    li = list(li)
-    gam = list(gam)
-    row = li[0]
-    left_right = li[1]
-    distance = li[2]
-    if left_right == 0 :
-        gam[row-1] = gam[row-1][distance:] + gam[row-1][:distance]
-    elif left_right == 1:
-        gam[row-1] = gam[row-1][-distance:] + gam[row-1][:-distance]
-    return gam
-
-
 for i in range(m) :
-    a = rotate(b[i], a)
+    h, s, k = map(int, input().split())
+    if s == 0:
+        for _ in range(k) :
+            a[h-1].append(a[h-1].pop(0))
+    else:
+        for _ in range(k) :
+            a[h-1].insert(0, a[h-1].pop())
+res=0
+s=0
+e=n-1
 
-mid = n//2
-total = 0
 for i in range(n) :
-    dist = abs(mid-i)
-    total += sum(a[i][mid-dist:mid+dist+1])
-
-print(total)
+    for j in range(s, e+1) :
+        res += a[i][j]
+    if i < n//2 :
+        s += 1
+        e -= 1
+    else:
+        s -= 1
+        e += 1
+print(res)
 
 # 강사님 풀이
-
+'''
 n = int(input())
 a = [list(map(int, input().split())) for _ in range(n)]
 
@@ -58,3 +55,4 @@ for i in range(n) :
         s -= 1
         e += 1
 print(res)
+'''
