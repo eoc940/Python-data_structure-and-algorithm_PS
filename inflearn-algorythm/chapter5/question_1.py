0cd8952e47bd87a50fe3e803785b2d432d1cda75
eoc940 <hgk0000@naver.com>
2021-02-28 15:52:21 +0900

    feat: 가장 큰 수(스택) 학습

---
+++ inflearn-algorythm/chapter5/question_1.py
@@ -0,0 +1,14 @@
+# 가장 큰 수
+
+num, m = map(int, input().split())
+num = list(map(int, str(num))) # 이 방법 알아두기!!
+stack = []
+for x in num:
+    while stack and m>0 and stack[-1]<x:
+        stack.pop()
+        m -= 1
+    stack.append(x)
+if m != 0:
+    stack = stack[:-m]
+res = "".join(map(str, stack)) # for 안쓰고 join으로 출력 방법
+print(res)
