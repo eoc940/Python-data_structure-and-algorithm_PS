ef6a2652363ecdfec1c10afa71ce0bae10bb47c7
eoc940 <hgk0000@naver.com>
2021-03-20 21:11:14 +0900

    feat: 경로 탐색(그래프 DFS) 다시 풀어보기

--- inflearn-algorythm/chapter6/question_13.py
+++ inflearn-algorythm/chapter6/question_13.py
@@ -1,5 +1,5 @@
 # 경로 탐색(그래프 DFS)
-
+'''
 import sys
 
 def DFS(L) :
@@ -30,3 +30,52 @@ for i in range(m) :
 ch[1] = 1
 DFS(1)
 print(cnt)
+
+'''
+# 다시 풀어보기
+
+def DFS(L) :
+    global cnt
+    if L==n:
+        for x in res:
+            print(x, end=" ")
+        print()
+        cnt += 1
+    else :
+        for i in range(1, n+1) :
+            if ch[i] == 0 and g[L][i] == 1:
+                ch[i] = 1
+                res.append(i)
+                DFS(i)
+                res.pop()
+                ch[i] = 0
+
+n, m = map(int, input().split())
+g = [[0]*(n+1) for _ in range(n+1)]
+cnt = 0
+ch = [0]*(n+1)
+res = []
+res.append(1)
+for i in range(m) :
+    a, b = map(int, input().split())
+    g[a][b] = 1
+ch[1] = 1
+DFS(1)
+print(cnt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
