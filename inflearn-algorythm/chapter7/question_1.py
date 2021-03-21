da79f3da30fe2ced4a0f193ebe92c9e950387a44
eoc940 <hgk0000@naver.com>
2021-03-21 12:17:27 +0900

    feat: 최대점수 구하기(DFS) 다시풀기

--- inflearn-algorythm/chapter7/question_1.py
+++ inflearn-algorythm/chapter7/question_1.py
@@ -1,6 +1,6 @@
 # 최대점수 구하기(DFS)
 import sys
-
+'''
 def DFS(L, ssum, tsum) :
     global maximum
     if tsum > m :
@@ -23,3 +23,46 @@ for i in range(n) :
 
 DFS(0,0,0)
 print(maximum)
+
+
+'''
+
+# 다시 풀기
+
+def DFS(L, score, time) :
+    global res
+    if time > m :
+        return
+
+    if L==n:
+        if score > res :
+            res = score
+    else :
+        DFS(L+1, score + score_li[L], time + time_li[L])
+        DFS(L+1, score, time)
+
+n, m = map(int, input().split())
+score_li = []
+time_li = []
+res = 0
+for i in range(n) :
+    a, b = map(int, input().split())
+    score_li.append(a)
+    time_li.append(b)
+
+DFS(0,0,0)
+print(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
