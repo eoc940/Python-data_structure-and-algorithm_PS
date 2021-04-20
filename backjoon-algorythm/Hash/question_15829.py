73fa4cd2f7b545133c60a22b2d74e43b3be845e7
eoc940 <hgk0000@naver.com>
2021-04-20 22:58:31 +0900

    feat: 백준 15829번 문제(해시) 학습

---
+++ backjoon-algorythm/Hash/question_15829.py
@@ -0,0 +1,20 @@
+# Hashing
+
+import sys
+
+n = int(sys.stdin.readline())
+alpha = sys.stdin.readline()
+hashing = dict()
+result = 0
+mod = 1234567891
+
+for i in range(26) :
+    hashing[chr(97+i)] = i+1
+
+for j in range(n) :
+    result += hashing.get(alpha[j])*(31**j)
+
+print(result%mod)
+
+
+
