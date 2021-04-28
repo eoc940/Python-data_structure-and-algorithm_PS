f07d3b0f7e97ddea0799f75fbea7121558018b3f
eoc940 <hgk0000@naver.com>
2021-04-28 23:20:37 +0900

    feat: 백준 10872 팩토리얼 문제 학습

---
+++ backjoon-algorythm/Recursion/factorial.py
@@ -0,0 +1,12 @@
+# 10872 팩토리얼
+
+import sys
+
+def factorial(num) :
+    if num==0 :
+        return 1
+    return num * factorial(num-1)
+
+n = int(sys.stdin.readline().rstrip())
+
+print(factorial(n))
