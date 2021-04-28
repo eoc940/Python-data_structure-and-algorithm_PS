24791a35ba102c8843e08e5a4a8e8f65bba50caa
eoc940 <hgk0000@naver.com>
2021-04-29 00:25:54 +0900

    feat: 프로그래머스 카카오 괄호변환 문제 학습

--- programmers-algorythm/kakao/bracket.py
+++ programmers-algorythm/kakao/bracket.py
@@ -1,5 +1,34 @@
 # 괄호 변환
 
 def solution(p):
-    answer = ''
-    return answer
+    open = 0
+    close = 0
+    right = True
+    if p=="" :
+        return ""
+
+    for x in p :
+        if x=="(" :
+            open += 1
+        else :
+            close += 1
+        if open < close :
+            right = False
+        if open == close :
+            if right :
+                return p[:open+close] + solution(p[open+close:])
+            else : #올바른 괄호 문자열이 아님
+                convert = ""
+                convert += "("
+                convert += solution(p[open + close:])
+                convert += ")"
+                tmp = p[:open+close]
+                remove_fb = tmp[1:len(tmp) - 1]
+                for y in remove_fb :
+                    if y=="(" :
+                        convert += ")"
+                    else:
+                        convert += "("
+                return convert
+
+print(solution("(()())()"))
