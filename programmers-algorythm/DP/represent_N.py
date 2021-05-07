ffe6f5017d1c0aaaa78c6abe078b485f353a4903
eoc940 <hgk0000@naver.com>
2021-05-07 22:56:16 +0900

    feat: 프로그래머스 N으로 표현 학습

---
+++ programmers-algorythm/DP/represent_N.py
@@ -0,0 +1,32 @@
+# N으로 표현
+
+def solution(N, number):
+    answer = 0
+    dy = []
+    recur = 1
+    while True :
+        tmp = set()
+        tmp.add(int(str(N) * recur))
+        for i in range(len(dy)) :
+            for j in dy[i] :
+                for k in dy[len(dy)-i-1] :
+                    tmp.add(j+k)
+                    tmp.add(j-k)
+                    tmp.add(j*k)
+                    if k!=0 :
+                        tmp.add(j//k)
+        recur += 1
+        dy.append(list(tmp))
+        if len(dy)==9:
+            answer = -1
+            break
+        if number in dy[-1] :
+            answer = len(dy)
+            break
+
+    return answer
+
+print(solution(6,999))
+
+
+
