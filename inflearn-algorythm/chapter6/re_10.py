f4f5238abbe0f4c5f7f327397c94f88a78bc260f
eoc940 <hgk0000@naver.com>
2021-06-03 18:59:57 +0900

    feat: 조합 구하기(DFS) 학습

---
+++ inflearn-algorythm/chapter6/re_10.py
@@ -0,0 +1,29 @@
+# 조합 구하기
+
+import sys
+
+n,m = map(int, sys.stdin.readline().split())
+
+def dfs(v) :
+    if v==m :
+        tmp = []
+        for x in ch :
+            if x != 0 :
+                tmp.append(x)
+        group.append(tmp)
+    else :
+        for i in range(n) :
+            if i+1 not in ch and i+1 > ch[v-1] :
+                ch[v] = i+1
+                dfs(v+1)
+                ch[v] = 0
+
+ch = [0]*n
+group = []
+dfs(0)
+for x in group :
+    for y in x :
+        print(y, end=" ")
+    print()
+print(len(group))
+
