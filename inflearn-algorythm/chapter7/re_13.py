517ed5075b17d0eda1beb77a5c5e089cd59b1f05
eoc940 <hgk0000@naver.com>
2021-06-09 19:04:05 +0900

    feat: 섬나라 아일랜드(DFS) 학습

--- inflearn-algorythm/chapter7/re_13.py
+++ inflearn-algorythm/chapter7/re_13.py
@@ -19,6 +19,13 @@ def bfs(x,y) :
                 island[xx][yy] = 0
                 dq.append((xx,yy))
 
+def dfs(x,y) :
+    island[x][y] = 0
+    for i in range(8):
+        xx = x + dx[i]
+        yy = y + dy[i]
+        if 0 <= xx < n and 0 <= yy < n and island[xx][yy] == 1:
+            dfs(xx,yy)
 
 dx = [-1,-1,-1,0,1,1,1,0]
 dy = [-1,0,1,1,1,0,-1,-1]
@@ -28,7 +35,8 @@ for i in range(n) :
     for j in range(n):
         if island[i][j]==1 :
             #print(i,j)
-            bfs(i,j)
+            #bfs(i,j)
+            dfs(i,j)
             cnt += 1
 
 print(cnt)
