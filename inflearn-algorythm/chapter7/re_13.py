93f6fd022c50d0cabef596b8f5a18780c4529f4f
eoc940 <hgk0000@naver.com>
2021-06-09 19:00:50 +0900

    feat: 섬나라 아일랜드(BFS) 학습

---
+++ inflearn-algorythm/chapter7/re_13.py
@@ -0,0 +1,36 @@
+# 섬나라 아일랜드(BFS활용)
+
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+island = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
+def bfs(x,y) :
+    dq = deque()
+    island[x][y] = 0
+    dq.append((x,y))
+    while dq :
+        tmp = dq.popleft()
+        for i in range(8) :
+            xx = tmp[0] + dx[i]
+            yy = tmp[1] + dy[i]
+            if 0<=xx<n and 0<=yy<n and island[xx][yy] == 1 :
+                island[xx][yy] = 0
+                dq.append((xx,yy))
+
+
+dx = [-1,-1,-1,0,1,1,1,0]
+dy = [-1,0,1,1,1,0,-1,-1]
+cnt = 0
+
+for i in range(n) :
+    for j in range(n):
+        if island[i][j]==1 :
+            #print(i,j)
+            bfs(i,j)
+            cnt += 1
+
+print(cnt)
+
+
