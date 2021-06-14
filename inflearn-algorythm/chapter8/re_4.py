399046b1d732664847c3bca3e665a9384a7bd13a
eoc940 <hgk0000@naver.com>
2021-06-14 16:20:35 +0900

    feat: 최대 부분 증가수열(DP) 학습

---
+++ inflearn-algorythm/chapter8/re_4.py
@@ -0,0 +1,24 @@
+# 최대 부분 증가수열
+
+import sys
+
+n = int(sys.stdin.readline())
+nums = list(map(int, sys.stdin.readline().split()))
+
+dp = [0]*n
+
+dp[0] = 1
+
+for i in range(1, n) :
+    _max = 0
+    for j in range(i-1,0,-1) :
+        if nums[i] > nums[j] and dp[j] > _max:
+            _max = dp[j]
+    dp[i] = _max+1
+
+print(max(dp))
+
+
+
+
+
