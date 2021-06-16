d30f043595de4a97fe52e12670198366a6ea1ff4
eoc940 <hgk0000@naver.com>
2021-06-16 10:26:32 +0900

    feat: 백준 17142 BFS 다른풀이

--- backjoon-algorythm/DFS_BFS/question_17142.py
+++ backjoon-algorythm/DFS_BFS/question_17142.py
@@ -5,12 +5,42 @@ from collections import deque
 from itertools import combinations
 import copy
 
+def BFS(combin, left):
+    global time
+    dq = deque(combin)
+    time_cnt = 0
+    checked = [[0]*n for _ in range(n)]
+    for x, y in combin:
+        checked[x][y] = 1
+
+    while dq:
+        if not left: break #처음부터 left가 0일수 있기 때문에 바로 break
+        time_cnt += 1
+        if time_cnt >= time : return float('inf')
+        for _ in range(len(dq)):
+            x,y = dq.popleft()
+            for i in range(4):
+                xx = x + dx[i]
+                yy = y + dy[i]
+                if 0<=xx<n and 0<=yy<n and location[xx][yy] != 1 and not checked[xx][yy]:
+                    checked[xx][yy] = 1
+                    dq.append((xx,yy))
+                    if location[xx][yy] == 0 :
+                        left -= 1
+    if not left:
+        return time_cnt
+    else:
+        return float('inf')
+
+
+
 n, m = map(int, sys.stdin.readline().split())
 location = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 
 dx = [-1,0,1,0]
 dy = [0,1,0,-1]
 
+time = float('inf')
 virus = []
 not_infected = 0
 
@@ -18,55 +48,17 @@ for i in range(n):
     for j in range(n):
         if location[i][j]==2:
             virus.append((i,j))
+        if location[i][j]==0:
+            not_infected += 1
 
-combi = combinations(virus, m)
-#print(combi)
+combin = combinations(virus, m)
 
-answer = float('inf')
+for c in combin:
+    time = min(time, BFS(c, not_infected))
 
-for x in combi:
-    time = [[0] * n for _ in range(n)]
-    loca = copy.deepcopy(location)
-    dq = deque(x)
-    result = 0
-    while dq:
-        tmp = dq.popleft()
-        #print(tmp)
-        for i in range(4):
-            xx = dx[i] + tmp[0]
-            yy = dy[i] + tmp[1]
-            if 0<=xx<n and 0<=yy<n and loca[xx][yy]==0 and time[xx][yy]==0 :
-                dq.append((xx,yy))
-                loca[xx][yy] = 2
-                time[xx][yy] = time[tmp[0]][tmp[1]] + 1
-        if not dq :
-            result = time[tmp[0]][tmp[1]]
-    flag = 1
-    for j in range(n):
-        for k in range(n):
-            if loca[j][k] == 0:
-                flag = 0
+if time==float('inf'):
+    time = -1
 
-    if flag == 0:
-        result = -1
-        # for a in range(n):
-        #     for b in range(n):
-        #         if time[a][b] > result:
-        #             result = time[a][b]
-    # else :
-    #     result = -1
-
-    if result != -1 :
-        if answer > result:
-            answer = result
-    #print(result)
-
-
-if answer==float('inf') :
-    print(-1)
-else :
-    print(answer)
-
-
+print(time)
 
 
