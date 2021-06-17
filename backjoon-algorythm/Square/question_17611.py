c8ffb9590d9d9890beb79704c33bfa40457e3614
eoc940 <hgk0000@naver.com>
2021-06-17 23:55:00 +0900

    feat: 백준 직각다각형 다른 풀이

--- backjoon-algorythm/Square/question_17611.py
+++ backjoon-algorythm/Square/question_17611.py
@@ -1,33 +1,49 @@
 # 직각다각형
 
 import sys
-import heapq
 from collections import defaultdict
 
 n = int(sys.stdin.readline())
 
-line = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-count_x = defaultdict(int)
-count_y = defaultdict(int)
-flag = False
+data = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+data.append(data[0])
 
-if line[0][0] == line[1][0] :
-    flag = True
-else :
-    last = line.pop()
-    line.insert(0,last)
-for i in range(len(line)-1):
-    x_maxi = max(line[i-1][0], line[i][0])
-    x_mini = min(line[i-1][0], line[i][0])
-    y_maxi = max(line[i][1], line[i+1][1])
-    y_mini = min(line[i][1], line[i+1][1])
-    x_total, y_total = 0, 0
-    for a in range(x_mini, x_maxi) :
-        count_x[a+0.5] += 1
-    for b in range(y_mini, y_maxi) :
-        count_y[b+0.5] += 1
+num_dict_x = defaultdict(int)
+num_dict_y = defaultdict(int)
 
-#print(count_x)
-#print(count_y)
-answer = max(max(count_x.values()), max(count_y.values()))
-print(answer)
+pre_x, pre_y = data[0][0], data[0][1]
+for i in range(1, len(data)):
+    x,y = data[i][0], data[i][1]
+    if x==pre_x :
+        start = min(y,pre_y)
+        end = max(y,pre_y)
+        num_dict_y[start] += 1
+        num_dict_y[end] -= 1
+
+    if y==pre_y:
+        start = min(x,pre_x)
+        end = max(x,pre_x)
+        num_dict_x[start] += 1
+        num_dict_x[end] -= 1
+
+    pre_x, pre_y = x, y
+# print(num_dict_x)
+# print(num_dict_y)
+num_dict_x = sorted(num_dict_x.items(), key=(lambda x:x[0]))
+num_dict_y = sorted(num_dict_y.items(), key=(lambda x:x[0]))
+# print(num_dict_x)
+# print(num_dict_y)
+
+max_x = 0
+tmp_x = 0
+for k, v in num_dict_x:
+    tmp_x += v
+    max_x = max(max_x, tmp_x)
+
+max_y = 0
+tmp_y = 0
+for k, v in num_dict_y:
+    tmp_y += v
+    max_y = max(max_y, tmp_y)
+
+print(max(max_x, max_y))
