cd9a0082e8437c2357f0e4978f217f52aab8ba06
eoc940 <hgk0000@naver.com>
2021-06-22 23:18:30 +0900

    feat: DFS로 재구현

--- inflearn-algorythm/chapter8/re_13.py
+++ inflearn-algorythm/chapter8/re_13.py
@@ -1,10 +1,53 @@
 # 플로이드 워셜 알고리즘
 
 import sys
+from collections import defaultdict, deque
 
 n,m = map(int, sys.stdin.readline().split())
-connect = [[float('inf')]*(n+1) for _ in range(n+1)]
+connect = defaultdict(list)
 
+for _ in range(m):
+    st, en, cost = map(int, sys.stdin.readline().split())
+    connect[st].append((en, cost))
+
+#print(connect)
+
+def dfs(x, y, total) :
+    global answer
+    if x==y:
+        #print(total, answer, end=" ")
+        if answer > total:
+            answer = total
+    else :
+        for x in connect[x]:
+            en, cost = x[0], x[1]
+            if ch[en] == 0:
+                ch[en] = 1
+                dfs(en, y, total + cost)
+                ch[en] = 0
+
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        answer = float('inf')
+        ch = [0]*(n+1)
+        ch[i] = 1
+        dfs(i,j,0)
+
+        if answer == float('inf'):
+            print("M", end=" ")
+        else :
+            print(answer, end=" ")
+    print()
+
+
+
+
+
+
+
+
+'''
 for _ in range(m):
     st, en, cost = map(int, sys.stdin.readline().split())
     connect[st][en] = cost
@@ -29,3 +72,4 @@ for i in range(1,n+1):
             print(connect[i][j], end=" ")
     print()
 
+'''
