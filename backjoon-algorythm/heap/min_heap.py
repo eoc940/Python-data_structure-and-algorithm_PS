a34ee1a8d427ed630dba3197407cfce5c3b27b30
eoc940 <hgk0000@naver.com>
2021-06-24 18:37:34 +0900

    feat: 최소힙 학습

---
+++ backjoon-algorythm/heap/min_heap.py
@@ -0,0 +1,20 @@
+# 최소 힙
+
+import sys, heapq
+
+n = int(sys.stdin.readline())
+nums = [int(sys.stdin.readline()) for _ in range(n)]
+
+li = []
+
+for x in nums :
+    if x == 0 :
+        if len(li) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(li))
+    else :
+        heapq.heappush(li, x)
+
+
+
