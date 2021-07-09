b993087a928e32723b1e44ea06fdc76d4e7f3fbc
eoc940 <hgk0000@naver.com>
2021-07-09 20:35:21 +0900

    feat: 프로그래머스 숫자 문자열과 영단어 학습

---
+++ programmers-algorythm/kakao/2021_kakao_intern/num_string_and_english_word.py
@@ -0,0 +1,15 @@
+# 숫자 문자열과 영단어
+
+def solution(s):
+    answer = 0
+    mapping = {"zero":0,"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9}
+    for key, val in mapping.items():
+        s = s.replace(key, str(val))
+    answer = int(s)
+
+
+    return answer
+
+s = "123"
+print(solution(s))
+
