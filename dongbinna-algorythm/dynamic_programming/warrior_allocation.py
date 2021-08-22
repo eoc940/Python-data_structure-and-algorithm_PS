fd66ffe8b7380d9cf5dbbf2c9a4837d7d5959f58
eoc940 <hgk0000@naver.com>
2021-08-22 19:07:17 +0900

    feat: 병사 배치하기(다이나믹 프로그래밍) 학습

---
+++ dongbinna-algorythm/dynamic_programming/warrior_allocation.py
@@ -0,0 +1,23 @@
+# 병사 배치하기
+
+import sys
+
+n = int(sys.stdin.readline())
+power = list(map(int, sys.stdin.readline().split()))
+dp = [1]*len(power)
+
+for i in range(1, len(dp)):
+    if power[i-1] > power[i]:
+        dp[i] = max(dp[i-1] + 1, dp[i])
+    else :
+        dp[i] = dp[i-1]
+print(dp)
+print(dp[-1])
+
+'''
+answer = 0
+for i in range(1, len(power)):
+    if power[i-1] < power[i]:
+        answer += 1
+print(answer)
+'''
