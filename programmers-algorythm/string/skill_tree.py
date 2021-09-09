c646644bc8c0ada2cb1b40ceea0add65d2bb7306
eoc940 <hgk0000@naver.com>
2021-09-09 21:20:47 +0900

    feat: 프로그래머스 스킬트리 학습

---
+++ programmers-algorythm/string/skill_tree.py
@@ -0,0 +1,19 @@
+# 스킬트리
+
+def solution(skill, skill_trees):
+    answer = 0
+    for candi in skill_trees:
+        ch = list()
+        for sk in skill:
+            loca = candi.find(sk)
+            if loca == -1 : loca = 100
+            ch.append(loca)
+        sorted_ch = sorted(ch)
+
+        if ch == sorted_ch: answer += 1
+    return answer
+
+skill = "CBD"
+skill_trees = ["BACDE", "CBADF", "AECB", "BDA"]
+print(solution(skill, skill_trees))
+
