8be2b87ae04da2a1b96598566d7074cccaa6c07e
eoc940 <hgk0000@naver.com>
2021-09-12 20:26:08 +0900

    feat: 프로그래머스 쿼드 압축 후 개수 세기 학습

---
+++ programmers-algorythm/figure/quard.py
@@ -0,0 +1,36 @@
+# 쿼드 압축 후 개수 세기
+
+
+def validate(x, y, leng, arr): # 4,0 이라고 가정
+    li = []
+    if arr[x][y] == -1: return -1
+    stan = arr[x][y]
+    for i in range(x, x+leng):
+        for j in range(y, y+leng):
+            if arr[i][j] != stan:
+                return -1
+    for i in range(x, x + leng):
+        for j in range(y, y + leng):
+            arr[i][j] = -1
+    return stan
+
+def solution(arr):
+    zero, one = 0, 0
+    leng = len(arr)
+    divide = []
+    while leng > 0:
+        divide.append(leng)
+        leng = leng//2
+
+    for div in divide: # div == 4
+        for i in range(len(arr)//div):
+            for j in range(len(arr) // div):
+                result = validate(div*i, div*j,div, arr)
+                if result == 0: zero += 1
+                elif result == 1: one += 1
+
+    return [zero, one]
+
+arr = [[1,1,0,0],[1,0,0,0],[1,0,0,1],[1,1,1,1]]
+print(solution(arr))
+
