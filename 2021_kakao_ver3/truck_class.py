8e230598ab805bdf48f4ffb3373d8ed4ec9eccc4
eoc940 <hgk0000@naver.com>
2021-09-24 23:34:34 +0900

    feat: 프로그래머스 2021 카카오 2차 코딩 테스트 학습(코드 개선 -> 점수 향상)

--- 2021_kakao_ver3/truck_class.py
+++ 2021_kakao_ver3/truck_class.py
@@ -1,6 +1,14 @@
-
-# 10으로 돌려보겠다
-MAX_BIKE = 10
+'''
+0: 6초간 아무것도 하지 않음
+1: 위로 한 칸 이동
+2: 오른쪽으로 한 칸 이동
+3: 아래로 한 칸 이동
+4: 왼쪽으로 한 칸 이동
+5: 자전거 상차
+6: 자전거 하차
+'''
+# 트럭의 최대 자전거를 2로 제한
+MAX_BIKE = 2
 
 class truck:
     tid:int = None
@@ -9,12 +17,14 @@ class truck:
     lid:int = None
     bike:int = None
     map_leng:int = None
+    command:list = None
 
-    def __init__(self, tid, lid, bike, map_leng):
+    def __init__(self, tid, lid, bike, map_leng, command):
         self.tid = tid
         self.lid = lid
         self.bike = bike
         self.map_leng = map_leng
+        self.command = command
         self.convert()
 
     def convert(self):
@@ -25,30 +35,30 @@ class truck:
     def up(self):
         if self.row > 0:
             self.row -= 1
-            return 1
+            self.command.append(1)
 
     def right(self):
         if self.col < self.map_leng - 1:
             self.col += 1
-            return 2
+            self.command.append(2)
 
     def down(self):
         if self.row < self.map_leng - 1:
             self.row += 1
-            return 3
+            self.command.append(3)
 
     def left(self):
         if self.col > 0:
             self.col -= 1
-            return 4
+            self.command.append(4)
 
     def load(self):
         if self.bike < MAX_BIKE:
             self.bike += 1
-            return 5
+            self.command.append(5)
 
     def land(self):
         if self.bike > 0:
             self.bike -= 1
-            return 6
+            self.command.append(6)
 
--- 2021_kakao_ver3/truck_operation.py
+++ 2021_kakao_ver3/truck_operation.py
@@ -5,7 +5,7 @@ import random
 
 # API
 
-token = '356384a925546199b8a9c8cce5d0af5a'
+token = '694673e628d83ed45979e87671de0108'
 url = 'https://kox947ka1a.execute-api.ap-northeast-2.amazonaws.com/prod/users'
 
 def startAPI(problem):
@@ -70,98 +70,94 @@ def convert(location_json):
         locations[MAX_LENG-1-y][x] = location["located_bikes_count"]
     return locations
 
-def move(tr, locations):
-    _truck = truck(tr["id"], tr["location_id"], tr["loaded_bikes_count"], MAX_LENG)
+def move_one(tr, locations):
+    _truck = truck(tr["id"], tr["location_id"], tr["loaded_bikes_count"], MAX_LENG, [])
     command = []
-
-    near = [_truck.row, _truck.col]
-    max_dist = 0
-    # 가장 먼 부족한 곳 탐색
-    # print("location :", locations)
+    max_place = []
+    min_place = []
     for i in range(MAX_LENG):
         for j in range(MAX_LENG):
-            if locations[i][j] < AVG_BIKE and not check[i][j]:
-                dist = abs(_truck.row - i) + abs(_truck.col - j)
-                if max_dist < dist and dist <= 6 and [i,j] != [_truck.row, _truck.col]:
-                    max_dist = dist
-                    near = [i, j]
-                    check[i][j] = True
+            max_place.append((locations[i][j],i,j))
+            min_place.append((locations[i][j], i, j))
+    max_place.sort(key=lambda x : x[0], reverse=True)
+    min_place.sort(key=lambda x : x[0])
 
-    # 거리 8 이내의 가장 먼 곳이 탐색됨
-    down = near[0] - _truck.row
-    right = near[1] - _truck.col
-    move_list = []
-    if down > 0:
-        for _ in range(down):
-            move_list.append("down")
-    else: # up
-        for _ in range(-down):
-            move_list.append("up")
-    if right > 0:
-        for _ in range(right):
-            move_list.append("right")
-    else: # left
-        for _ in range(-right):
-            move_list.append("left")
+    if _truck.bike > 0: # 가지고 있다면 없는 곳으로 간다
+        bike_cnt, row, col = min_place[_truck.tid]
+        # 1,3 -> 3,4
+        up = _truck.row - row
+        left = _truck.col - col
+        if up > 0:
+            for _ in range(up):
+                _truck.up()
+        else:
+            for _ in range(-up):
+                _truck.down()
+        if left > 0:
+            for _ in range(left):
+                _truck.left()
+        else:
+            for _ in range(-left):
+                _truck.right()
+        for _ in range(_truck.bike):
+            _truck.land()
+            locations[row][col] += 1
 
-    random.shuffle(move_list)
-    print([_truck.row, _truck.col],near, locations[near[0]][near[1]],move_list)
+    else: # 최대 많은 곳으로 가서 싣고 없는 곳으로 간다
+        bike_cnt, row, col = max_place[_truck.tid]
+        # 1,3 -> 3,4
+        up = _truck.row - row
+        left = _truck.col - col
+        if up > 0:
+            for _ in range(up):
+                _truck.up()
+        else:
+            for _ in range(-up):
+                _truck.down()
+        if left > 0:
+            for _ in range(left):
+                _truck.left()
+        else:
+            for _ in range(-left):
+                _truck.right()
+        if locations[_truck.row][_truck.col] >= AVG_BIKE:
+            load_cnt = locations[_truck.row][_truck.col] + 1 - AVG_BIKE
+            for _ in range(load_cnt):
+                _truck.load()
+                locations[row][col] -= 1
+
+        bike_cnt, row, col = min_place[_truck.tid]
+        # 1,3 -> 3,4
+        up = _truck.row - row
+        left = _truck.col - col
+        if up > 0:
+            for _ in range(up):
+                _truck.up()
+        else:
+            for _ in range(-up):
+                _truck.down()
+        if left > 0:
+            for _ in range(left):
+                _truck.left()
+        else:
+            for _ in range(-left):
+                _truck.right()
+        for _ in range(_truck.bike):
+            _truck.land()
+            locations[row][col] += 1
+
+
+    print("command :", _truck.command)
+    return {"truck_id":_truck.tid, "command":_truck.command[:10]}
+
+def move_two(tr, locations):
+    _truck = truck(tr["id"], tr["location_id"], tr["loaded_bikes_count"], MAX_LENG, [])
     command = []
-    if locations[_truck.row][_truck.col] >= AVG_BIKE:
-        command.append(_truck.load())
-        locations[_truck.row][_truck.col] -= 1
-    elif locations[_truck.row][_truck.col] < AVG_BIKE-1:
-        command.append(_truck.land())
-        locations[_truck.row][_truck.col] += 1
-    for mv in move_list:
-        if mv=="up":
-            command.append(_truck.up())
-        elif mv=="down":
-            command.append(_truck.down())
-        elif mv=="left":
-            command.append(_truck.left())
-        elif mv=="right":
-            command.append(_truck.right())
-        if locations[_truck.row][_truck.col] >= AVG_BIKE:
-            command.append(_truck.load())
-            locations[_truck.row][_truck.col] -= 1
-        elif locations[_truck.row][_truck.col] < AVG_BIKE - 1:
-            command.append(_truck.land())
-            locations[_truck.row][_truck.col] += 1
+    # print(locations)
 
-    if len(command) == 0:
-        if locations[_truck.row][_truck.col] >= AVG_BIKE:
-            command.append(_truck.load())
-            locations[_truck.row][_truck.col] -= 1
-        elif locations[_truck.row][_truck.col] < AVG_BIKE - 1:
-            command.append(_truck.land())
-            locations[_truck.row][_truck.col] += 1
-        for i in range(6):
-            num = random.randrange(1,5)
-            if num==1:
-                command.append(_truck.up())
-            elif num==2:
-                command.append(_truck.down())
-            elif num==3:
-                command.append(_truck.left())
-            else: # 4
-                command.append(_truck.right())
-            if locations[_truck.row][_truck.col] >= AVG_BIKE:
-                command.append(_truck.load())
-                locations[_truck.row][_truck.col] -= 1
-            elif locations[_truck.row][_truck.col] < AVG_BIKE - 1:
-                command.append(_truck.land())
-                locations[_truck.row][_truck.col] += 1
 
-    removed_none = []
-    for com in command:
-        if com != None: removed_none.append(com)
-
-    command = removed_none
-    # print("command :", command[:10])
-
+    print("command :", command)
     return {"truck_id":_truck.tid, "command":command[:10]}
-
 
 # 시나리오 1
 
@@ -172,14 +168,16 @@ auth_key = startAPI(1)
 while True:
     location_json = locationAPI(auth_key)
     locations = convert(location_json)
+    print(locations)
     check = [[False for _ in range(MAX_LENG)] for _ in range(MAX_LENG)]
     trucks = truckAPI(auth_key)
     commands = []
     for tr in trucks:
-        truck_action = move(tr, locations)
+        truck_action = move_one(tr, locations)
         commands.append(truck_action)
     status, time, fail_count, distance = simulateAPI(auth_key, commands)
     print("time :", time)
+    print("fail :", fail_count)
     if status == "finished":
         print("실패 :", fail_count)
         print("디폴트 성공률 :", 1077, "/", 1428)
@@ -201,7 +199,7 @@ print(scoreAPI(auth_key))
 #     trucks = truckAPI(auth_key)
 #     commands = []
 #     for tr in trucks:
-#         truck_action = move(tr, locations)
+#         truck_action = move_two(tr, locations)
 #         commands.append(truck_action)
 #     status, time, fail_count, distance = simulateAPI(auth_key, commands)
 #     print("time :", time)
